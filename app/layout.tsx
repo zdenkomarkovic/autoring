65ad45cddef905f8b66e158e1198725e473b8240
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-02-13 01:10:31 +0100

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import CallButton from "@/components/Callbutton";
+import CallButton from "@/components/CallButton";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import type { Metadata } from "next";
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Hero from "@/components/Hero";
 
-import VozniPark from "@/components/voznipark";
+import VozniPark from "@/components/VozniPark";
 
 export default function Home() {
   return (
