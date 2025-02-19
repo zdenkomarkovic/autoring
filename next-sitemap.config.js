30ad2b50ad6e15532b18ae5469a2d2d79bc8d0d9
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-02-19 22:37:26 +0100

    .

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://.kombiprevozputnikapozega.rs/", // Zameni svojim domenom
+  siteUrl: "https://kombiprevozputnikapozega.rs/", // Zameni svojim domenom
   generateRobotsTxt: true,
   sitemapSize: 5000,
 };
